243528bee89e6439413badd06f915352698e0848
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2024-06-10 13:05:37 +0700

    add few things

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 let startX, startY, endX, endY;
 let mode = "";
-let selectedShape = null;
 let shapes = [];
 
 canvas.addEventListener("mousedown", onMouseDown);
@@ -54,13 +53,13 @@ function getMousePos(event) {
 class Shape {
   constructor(type) {
     this.type = type;
+    this.points = [];
   }
 
   draw() {}
   translate(dx, dy) {}
-  rotate(angle) {}
+  rotate(angle, centerX, centerY) {}
   scale(sx, sy) {}
-  reflect(axis) {}
 }
 
 class Line extends Shape {
@@ -71,6 +70,10 @@ class Line extends Shape {
     this.x2 = x2;
     this.y2 = y2;
     this.algorithm = algorithm;
+    this.points = [
+      { x: x1, y: y1 },
+      { x: x2, y: y2 },
+    ];
   }
 
   draw() {
@@ -123,35 +126,37 @@ class Line extends Shape {
     this.y1 += dy;
     this.x2 += dx;
     this.y2 += dy;
+    this.updatePoints();
   }
 
-  rotate(angle) {
+  rotate(angle, centerX, centerY) {
     const rad = angle * (Math.PI / 180);
     const cos = Math.cos(rad);
     const sin = Math.sin(rad);
-    const nx1 = this.x1 * cos - this.y1 * sin;
-    const ny1 = this.x1 * sin + this.y1 * cos;
-    const nx2 = this.x2 * cos - this.y2 * sin;
-    const ny2 = this.x2 * sin + this.y2 * cos;
-    this.x1 = nx1;
-    this.y1 = ny1;
-    this.x2 = nx2;
-    this.y2 = ny2;
+
+    let x1 = this.x1 - centerX;
+    let y1 = this.y1 - centerY;
+    let x2 = this.x2 - centerX;
+    let y2 = this.y2 - centerY;
+
+    this.x1 = x1 * cos - y1 * sin + centerX;
+    this.y1 = x1 * sin + y1 * cos + centerX;
+    this.x2 = x2 * cos - y2 * sin + centerY;
+    this.y2 = x2 * sin + y2 * cos + centerY;
+    this.updatePoints();
   }
 
   scale(sx, sy) {
     this.x2 = this.x1 + (this.x2 - this.x1) * sx;
     this.y2 = this.y1 + (this.y2 - this.y1) * sy;
+    this.updatePoints();
   }
 
-  reflect(axis) {
-    if (axis === "x") {
-      this.y1 = -this.y1;
-      this.y2 = -this.y2;
-    } else {
-      this.x1 = -this.x1;
-      this.x2 = -this.x2;
-    }
+  updatePoints() {
+    this.points = [
+      { x: this.x1, y: this.y1 },
+      { x: this.x2, y: this.y2 },
+    ];
   }
 }
 
@@ -161,6 +166,7 @@ class Circle extends Shape {
     this.cx = cx;
     this.cy = cy;
     this.radius = radius;
+    this.points = this.calculatePoints();
   }
 
   draw() {
@@ -198,22 +204,51 @@ class Circle extends Shape {
   translate(dx, dy) {
     this.cx += dx;
     this.cy += dy;
+    this.points = this.calculatePoints();
   }
 
-  rotate(angle) {
-    // Rotation doesn't change the circle.
+  rotate(angle, centerX, centerY) {
+    const rad = angle * (Math.PI / 180);
+    const cos = Math.cos(rad);
+    const sin = Math.sin(rad);
+
+    let cx = this.cx - centerX;
+    let cy = this.cy - centerY;
+
+    this.cx = cx * cos - cy * sin + centerX;
+    this.cy = cx * sin + cy * cos + centerX;
+    this.points = this.calculatePoints();
   }
 
   scale(s) {
     this.radius *= s;
+    this.points = this.calculatePoints();
   }
 
-  reflect(axis) {
-    if (axis === "x") {
-      this.cy = -this.cy;
-    } else {
-      this.cx = -this.cx;
+  calculatePoints() {
+    const points = [];
+    let x = 0;
+    let y = this.radius;
+    let p = 1 - this.radius;
+    points.push({ x: this.cx + x, y: this.cy + y });
+    while (x < y) {
+      x++;
+      if (p < 0) {
+        p += 2 * x + 1;
+      } else {
+        y--;
+        p += 2 * (x - y) + 1;
+      }
+      points.push({ x: this.cx + x, y: this.cy + y });
+      points.push({ x: this.cx - x, y: this.cy + y });
+      points.push({ x: this.cx + x, y: this.cy - y });
+      points.push({ x: this.cx - x, y: this.cy - y });
+      points.push({ x: this.cx + y, y: this.cy + x });
+      points.push({ x: this.cx - y, y: this.cy + x });
+      points.push({ x: this.cx + y, y: this.cy - x });
+      points.push({ x: this.cx - y, y: this.cy - x });
     }
+    return points;
   }
 }
 
@@ -224,6 +259,7 @@ class Ellipse extends Shape {
     this.cy = cy;
     this.rx = rx;
     this.ry = ry;
+    this.points = this.calculatePoints();
   }
 
   draw() {
@@ -231,22 +267,24 @@ class Ellipse extends Shape {
   }
 
   drawEllipseMidpoint() {
-    let x = 0;
-    let y = this.ry;
     let rxSq = this.rx * this.rx;
     let rySq = this.ry * this.ry;
+    let x = 0;
+    let y = this.ry;
     let p1 = rySq - rxSq * this.ry + 0.25 * rxSq;
+    this.plotEllipsePoints(x, y);
+
     while (2 * rySq * x <= 2 * rxSq * y) {
-      this.plotEllipsePoints(x, y);
+      x++;
       if (p1 < 0) {
-        x++;
         p1 += 2 * rySq * x + rySq;
       } else {
-        x++;
         y--;
         p1 += 2 * rySq * x - 2 * rxSq * y + rySq;
       }
+      this.plotEllipsePoints(x, y);
     }
+
     let p2 =
       rySq * ((x + 0.5) * (x + 0.5)) + rxSq * ((y - 1) * (y - 1)) - rxSq * rySq;
     while (y >= 0) {
@@ -272,29 +310,68 @@ class Ellipse extends Shape {
   translate(dx, dy) {
     this.cx += dx;
     this.cy += dy;
+    this.points = this.calculatePoints();
   }
 
-  rotate(angle) {
+  rotate(angle, centerX, centerY) {
     const rad = angle * (Math.PI / 180);
     const cos = Math.cos(rad);
     const sin = Math.sin(rad);
-    const ncx = this.cx * cos - this.cy * sin;
-    const ncy = this.cx * sin + this.cy * cos;
-    this.cx = ncx;
-    this.cy = ncy;
+
+    let cx = this.cx - centerX;
+    let cy = this.cy - centerY;
+
+    this.cx = cx * cos - cy * sin + centerX;
+    this.cy = cx * sin + cy * cos + centerX;
+    this.points = this.calculatePoints();
   }
 
   scale(sx, sy) {
     this.rx *= sx;
     this.ry *= sy;
+    this.points = this.calculatePoints();
   }
 
-  reflect(axis) {
-    if (axis === "x") {
-      this.cy = -this.cy;
-    } else {
-      this.cx = -this.cx;
+  calculatePoints() {
+    const points = [];
+    let rxSq = this.rx * this.rx;
+    let rySq = this.ry * this.ry;
+    let x = 0;
+    let y = this.ry;
+    let p1 = rySq - rxSq * this.ry + 0.25 * rxSq;
+    points.push({ x: this.cx + x, y: this.cy + y });
+
+    while (2 * rySq * x <= 2 * rxSq * y) {
+      x++;
+      if (p1 < 0) {
+        p1 += 2 * rySq * x + rySq;
+      } else {
+        y--;
+        p1 += 2 * rySq * x - 2 * rxSq * y + rySq;
+      }
+      points.push({ x: this.cx + x, y: this.cy + y });
+      points.push({ x: this.cx - x, y: this.cy + y });
+      points.push({ x: this.cx + x, y: this.cy - y });
+      points.push({ x: this.cx - x, y: this.cy - y });
     }
+
+    let p2 =
+      rySq * ((x + 0.5) * (x + 0.5)) + rxSq * ((y - 1) * (y - 1)) - rxSq * rySq;
+    while (y >= 0) {
+      points.push({ x: this.cx + x, y: this.cy + y });
+      if (p2 > 0) {
+        y--;
+        p2 -= 2 * rxSq * y + rxSq;
+      } else {
+        y--;
+        x++;
+        p2 += 2 * rySq * x - 2 * rxSq * y + rxSq;
+      }
+      points.push({ x: this.cx - x, y: this.cy + y });
+      points.push({ x: this.cx + x, y: this.cy - y });
+      points.push({ x: this.cx - x, y: this.cy - y });
+    }
+    return points;
   }
 }
 
@@ -328,13 +405,13 @@ function applyTransformation() {
         shape.translate(dx, dy);
         break;
       case "rotate":
-        shape.rotate(angle);
+        shape.rotate(angle, canvas.width / 2, canvas.height / 2);
         break;
       case "scale":
         shape.scale(sx, sy);
         break;
       case "reflect":
-        shape.reflect(axis);
+        reflectShape(shape, axis);
         break;
     }
   });
@@ -374,3 +451,50 @@ function downloadCanvas() {
   link.download = "canvas.png";
   link.click();
 }
+
+function reflectShape(shape, axis) {
+  const center = findCenter(shape.points);
+  if (axis === "x") {
+    shape.points.forEach((point) => {
+      point.y = 2 * center.y - point.y;
+    });
+  } else {
+    shape.points.forEach((point) => {
+      point.x = 2 * center.x - point.x;
+    });
+  }
+  shape.updateFromPoints();
+}
+
+function findCenter(points) {
+  let xSum = 0;
+  let ySum = 0;
+  points.forEach((point) => {
+    xSum += point.x;
+    ySum += point.y;
+  });
+  return {
+    x: xSum / points.length,
+    y: ySum / points.length,
+  };
+}
+
+Shape.prototype.updateFromPoints = function () {
+  if (this.type === "line") {
+    this.x1 = this.points[0].x;
+    this.y1 = this.points[0].y;
+    this.x2 = this.points[1].x;
+    this.y2 = this.points[1].y;
+  } else if (this.type === "circle") {
+    this.cx = this.points[0].x;
+    this.cy = this.points[0].y;
+    this.radius = Math.sqrt(
+      (this.points[1].x - this.cx) ** 2 + (this.points[1].y - this.cy) ** 2
+    );
+  } else if (this.type === "ellipse") {
+    this.cx = this.points[0].x;
+    this.cy = this.points[0].y;
+    this.rx = Math.abs(this.points[1].x - this.cx);
+    this.ry = Math.abs(this.points[1].y - this.cy);
+  }
+};
